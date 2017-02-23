330ea5073254dda810b535d0e9f0230cf03f7aec
Nathan <njyoung94@gmail.com>
2017-02-22 23:07:49 -0500

    and another one

--- 2.py
+++ 2.py
@@ -1,10 +1,24 @@
+# Declare x and y
 x = 12
 y = 20
 
+# a
 print(x & y)
+
+# b
 print(~y)
+
+# c
 print(x | ~y)
+
+# d
 print(x >> 2)
+
+# e
 print((~y) << 1)
+
+# f
 print(y & (x << 1))
+
+# g
 print(x ^ (y >> 1))
--- 3.py
+++ 3.py
@@ -4,3 +4,24 @@ button_down = 1 << 1
 button_left = 1 << 2
 button_right = 1 << 3
 
+# Flip the state of the right button regardless of its initial state
+button_state ^= button_right
+print(bin(button_state))
+print(bin(button_up))
+print(bin(button_down))
+print(bin(button_left))
+print(bin(button_right))
+button_state |= button_left
+print(bin(button_state))
+# set down button to 0
+button_state &= ~button_left
+print(bin(button_state))
+
+#flip all states
+button_state = ~button_state
+print(bin(button_state))
+
+isDown = button_state & button_down
+print(isDown)
+isDown = isDown >> 3
+print(isDown)
